e2807b45f4798af2d23e241347f5b001b61b3773
phiseca <clarenceleroy2008@gmail.com>
2014-02-20 01:20:31 +0700

    Pembuatan main dan file plugin.yml

---
+++ ModularAce/src/io/github/phiseca/ModularAce.java
@@ -0,0 +1,17 @@
+package io.github.phiseca;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ModularAce  extends JavaPlugin{
+	
+	@Override
+	public void onEnable(){
+	
+	}
+	
+	@Override
+	public void onDisable(){
+		
+	}
+
+}
